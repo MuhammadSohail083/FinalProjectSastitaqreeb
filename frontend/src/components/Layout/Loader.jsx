f3d5e4ccdb167a6323e7360b1b6a30dbd5860165
MuhammadSohail083 <sohailhameed7473@gmail.com>
2023-06-22 21:42:47 +0500

    final project

---
+++ frontend/src/components/Layout/Loader.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+// import Lottie from "react-lottie";
+import './Loader.css';
+import animationData from "../../Assests/animations/24151-ecommerce-animation.json";
+
+const Loader = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <>
+
+    <div className="loader">
+    <div className="loader__dot"></div>
+    <div className="loader__dot"></div>
+    <div className="loader__dot"></div>
+  </div>
+    </>
+    // <div className="w-full h-screen flex items-center justify-center">
+    //   <Lottie options={defaultOptions} width={300} height={300} />
+    // </div>
+  );
+};
+
+export default Loader;
---
+++ frontend/src/server.js
@@ -0,0 +1,8 @@
+// export const server = "http://sasti.coderouting.com/api/v2";
+
+// export const backend_url = "http://sasti.coderouting.com";
+
+
+export const server = "http://localhost:8000/api/v2";
+
+export const backend_url = "http://localhost:8000";
